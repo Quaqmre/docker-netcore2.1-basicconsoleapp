ff4b4ff701ea0c8e90372bac8a269f329c35a98d
Quaqmre <tatarmehmetakif@gmail.com>
2019-03-03 01:21:44 +0300

    Basic console app with docker in the linux container

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Yeni_klasör
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
